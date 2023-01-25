34338e1585584946236e9736198057cd461fa027
Nidhish Chauhan <rch246chauhan@gmail.com>
2023-01-25 10:04:12 +0530

    Balance Retieval

--- Airdrop/index.js
+++ Airdrop/index.js
@@ -5,4 +5,18 @@ const wallet = new Keypair()
 const publicKey = new PublicKey(wallet._keypair.publicKey)
 const privateKey = wallet._keypair.secretKey
 
-console.log(publicKey,privateKey);
+const getBalance = async () => {
+    try {
+        const connection = new Connection(clusterApiUrl('devnet'), 'confirmed')
+        const walletBalance = await connection.getBalance(publicKey)
+        console.log("Wallet Balance: "+walletBalance);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const main=async()=>{
+    await getBalance()
+}
+
+main()
