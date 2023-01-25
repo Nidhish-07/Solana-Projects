4711599a3f8cf3bca63088b85ff07f61c3c86d70
Nidhish Chauhan <rch246chauhan@gmail.com>
2023-01-25 09:57:37 +0530

    Credentials retrieving

--- Airdrop/index.js
+++ Airdrop/index.js
@@ -2,7 +2,7 @@ import { Connection, PublicKey, clusterApiUrl, Keypair, LAMPORTS_PER_SOL } from 
 
 const wallet = new Keypair()
 
-const publicKey = wallet._keypair.publicKey
+const publicKey = new PublicKey(wallet._keypair.publicKey)
 const privateKey = wallet._keypair.secretKey
 
 console.log(publicKey,privateKey);
