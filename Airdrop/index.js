f4bd60d106b9462c64978bf76173163df6516993
Nidhish Chauhan <rch246chauhan@gmail.com>
2023-01-25 10:11:36 +0530

    balance adding

--- Airdrop/index.js
+++ Airdrop/index.js
@@ -3,19 +3,31 @@ import { Connection, PublicKey, clusterApiUrl, Keypair, LAMPORTS_PER_SOL } from 
 const wallet = new Keypair()
 
 const publicKey = new PublicKey(wallet._keypair.publicKey)
-const privateKey = wallet._keypair.secretKey
+// const privateKey = wallet._keypair.secretKey
 
 const getBalance = async () => {
     try {
         const connection = new Connection(clusterApiUrl('devnet'), 'confirmed')
         const walletBalance = await connection.getBalance(publicKey)
-        console.log("Wallet Balance: "+walletBalance);
+        console.log("Wallet Balance: " + walletBalance);
     } catch (error) {
         console.log(error);
     }
 }
 
-const main=async()=>{
+const airDropSol = async () => {
+    try {
+        const connection = new Connection(clusterApiUrl('devnet'), 'confirmed')
+        const airDropSignature = await connection.requestAirdrop(publicKey, 1 * LAMPORTS_PER_SOL)
+        await connection.confirmTransaction({ signature: airDropSignature })
+    } catch (error) {
+
+    }
+}
+
+const main = async () => {
+    await getBalance()
+    await airDropSol()
     await getBalance()
 }
 
