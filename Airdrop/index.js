fead2a349395cf45b78421902c677d8211208458
Nidhish Chauhan <rch246chauhan@gmail.com>
2023-01-25 09:57:23 +0530

    Credentials retrieving

--- Airdrop/index.js
+++ Airdrop/index.js
@@ -1,2 +1,8 @@
 import { Connection, PublicKey, clusterApiUrl, Keypair, LAMPORTS_PER_SOL } from "@solana/web3.js"
 
+const wallet = new Keypair()
+
+const publicKey = wallet._keypair.publicKey
+const privateKey = wallet._keypair.secretKey
+
+console.log(publicKey,privateKey);
